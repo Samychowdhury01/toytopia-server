09cc7f56cfaf430aa9d91a6b2061bfbe7bb36f7c
Samychowdhury01 <samychowdhury84@gmail.com>
2023-05-19 21:28:14 +0600

    added GET API for find data based on email

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // MongoDB
 
@@ -19,48 +19,61 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const toysCollection = client.db('toysDB').collection('toys')
+    const toysCollection = client.db("toysDB").collection("toys");
 
     // GET All Toys from server
-    app.get('/toys', async(req, res) =>{
-        const result = await toysCollection.find().sort({createdAt : -1}).limit(20).toArray()
-        res.send(result)
-    })
+    app.get("/toys", async (req, res) => {
+      const result = await toysCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(20)
+        .toArray();
+      res.send(result);
+    });
 
     // get a specific toy by ID
-    app.get('/toys/:id', async(req, res) =>{
-      const id = req.params.id
-      const query = {_id : new ObjectId(id)}
-      const result = await toysCollection.findOne(query)
-      res.send(result)
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
 
+    app.get('/my-Toys/', async(req, res) =>{
+      const email = req.query.email
+      const query = {email : email}
+      const result = await toysCollection.find(query).toArray()
+      res.send(result)
     })
 
     // POST a toy data
-    app.post('/addToy', async(req, res) =>{
-      const toy =  req.body
-      toy.createdAt = new Date()
-      const result = await toysCollection.insertOne(toy)
-      res.send(result)
-    })
+    app.post("/toys", async (req, res) => {
+      const toy = req.body;
+      toy.createdAt = new Date();
+      const result = await toysCollection.insertOne(toy);
+      res.send(result);
+    });
 
-
-    // search by name 
-    app.get('/searchToyByName/:name', async(req, res) =>{
-      const name = req.params.name
-     const result = await toysCollection.find({ toyName: { $regex: name, $options: 'i' } }).toArray()
-     res.send(result)
-    })
+    // search by name
+    app.get("/searchToyByName/:name", async (req, res) => {
+      const name = req.params.name;
+      const result = await toysCollection
+        .find({ toyName: { $regex: name, $options: "i" } })
+        .toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -68,15 +81,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("I am playing with action figure toy.");
+});
 
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('I am playing with action figure toy.')
-})
-
-app.listen(port, () =>{
-    console.log(`server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
